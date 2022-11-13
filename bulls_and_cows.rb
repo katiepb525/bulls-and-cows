6c6f6c6d74822d4b70b50f478d7f2fe30854947b
katie <katiepb525@gmail.com>
2022-11-13 14:30:47 -0600

    add player, computer, display, game classes

---
+++ bulls_and_cows.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# store all methods relating to player
+class Player
+end
+
+# store all methods relating to computer
+class Computer
+end
+
+# display input of player, output of computer, current round etc.
+class Display
+end
+
+# define rules for a game, define steps for a game, win/lose conditions etc.
+class NewGame
+end
