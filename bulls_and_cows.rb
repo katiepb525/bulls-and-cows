3312c9e18bece53a1df335ce5cfd51e0014cce1f
katie <katiepb525@gmail.com>
2022-11-20 17:05:53 -0600

    actually changed my mind sorry

--- bulls_and_cows.rb
+++ bulls_and_cows.rb
@@ -118,7 +118,7 @@ class NewGame
       puts "round #{current_round} / #{@MAX_TURNS}"
       # step 2: start with initial guess 1122
       if current_round == 1
-        @computer.guess = 1122.to_s
+        @computer.guess = 1122
       else
         com_play_round
       end
