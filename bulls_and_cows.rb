c82a3a3e37318677baf143057a3476dfec063cc8
katie <katiepb525@gmail.com>
2022-11-21 10:55:10 -0600

    pass in necessary args

--- bulls_and_cows.rb
+++ bulls_and_cows.rb
@@ -131,7 +131,7 @@ class NewGame
       if current_round == 1
         @computer.guess = 1122
       else
-        com_play_round(@computer.possible_codes)
+        com_play_round(@computer.true_possible_codes, @computer.guess, @player.master_code)
       end
       # announce guess
       puts "testing guess: #{@computer.guess}"
